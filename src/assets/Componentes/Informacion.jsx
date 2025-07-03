6b8c8bcdd8a850f29b4683f381738237fc007d78
OmarSerrano23 <oserranoayala@gmail.com>
2025-07-03 14:08:57 -0600

    lorem nuevo

--- src/assets/Componentes/Informacion.jsx
+++ src/assets/Componentes/Informacion.jsx
@@ -8,13 +8,20 @@ class Info extends React.Component {
   render() {
     return (
       <di>
-      <h3 onClick={() => this.ClickMe()}>
-        Lorem ipsum {this.props.city} dolor sit amet consectetur adipisicing
-        elit. Voluptatem, deleniti consequatur? Facere dignissimos
-        necessitatibus, recusandae rem voluptas eos! Obcaecati quas officiis
-        exercitationem nisi accusamus saepe laborum excepturi cumque fugiat modi{" "}
-        {this.props.age}.
-      </h3>
+        <h3 onClick={() => this.ClickMe()}>
+          Lorem ipsum {this.props.city} dolor sit amet consectetur adipisicing
+          elit. Voluptatem, deleniti consequatur? Facere dignissimos
+          necessitatibus, recusandae rem voluptas eos! Obcaecati quas officiis
+          exercitationem nisi accusamus saepe laborum excepturi cumque fugiat
+          modi {this.props.age}.
+        </h3>
+
+        <h3>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+          laudantium in neque harum iusto reprehenderit illum necessitatibus
+          porro sapiente, ipsam libero dolore eius rerum maiores asperiores,
+          ullam voluptatibus magnam sit.
+        </h3>
       </di>
     );
   }
